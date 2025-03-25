e773c362031c2653d6443bd818cdf31a1085ffda
Rio Ragil Ramdani <rioragilramdani7@gmail.com>
2025-03-25 12:09:40 +0700

    feat: Implementasi Main Screen dengan logika Tip Calculation

--- app/src/main/java/com/rioramdani0034/tipscalculate/ui/screen/MainScreen.kt
+++ app/src/main/java/com/rioramdani0034/tipscalculate/ui/screen/MainScreen.kt
@@ -1,24 +1,45 @@
 package com.rioramdani0034.tipscalculate.ui.screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.rioramdani0034.tipscalculate.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -27,7 +48,6 @@ fun MainScreen() {
 
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-
         topBar = {
             CenterAlignedTopAppBar(
                 colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
@@ -36,7 +56,7 @@ fun MainScreen() {
                 ),
                 title = {
                     Text(
-                        "Centered Top App Bar",
+                        text = stringResource(R.string.app_name),
                         maxLines = 1,
                         overflow = TextOverflow.Ellipsis
                     )
@@ -45,7 +65,7 @@ fun MainScreen() {
                     IconButton(onClick = { /* do something */ }) {
                         Icon(
                             imageVector = Icons.Filled.Home,
-                            contentDescription = "Localized description"
+                            contentDescription = stringResource(R.string.home),
                         )
                     }
                 },
@@ -53,7 +73,7 @@ fun MainScreen() {
                     IconButton(onClick = { /* do something */ }) {
                         Icon(
                             imageVector = Icons.Filled.Info,
-                            contentDescription = "Localized description"
+                            contentDescription = stringResource(R.string.info),
                         )
                     }
                 },
@@ -61,18 +81,128 @@ fun MainScreen() {
             )
         },
     ) { innerPadding ->
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(innerPadding)) {
-            Greeting("Android")
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            TipsCalculating()
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TipsCalculating() {
+    var nominalBayar by remember { mutableStateOf("") }
+    var totalBayar by remember { mutableDoubleStateOf(0.0) }
+    var expanded by remember { mutableStateOf(false) }
+    val tipOptions = listOf("10%", "20%", "25%", "50%")
+    var selectedTipOption by remember { mutableStateOf(tipOptions[0]) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Tip Calculator", style = MaterialTheme.typography.headlineMedium)
+
+        OutlinedTextField(
+            value = nominalBayar,
+            onValueChange = { nominalBayar = it },
+            label = { Text(text = stringResource(R.string.bill_amount)) },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            singleLine = true,
+            colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors()
+        )
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            OutlinedTextField(
+                value = selectedTipOption,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text(text = stringResource(R.string.tip_percent)) },
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                },
+                modifier = Modifier
+                    .menuAnchor()
+                    .fillMaxWidth(),
+                singleLine = true,
+                colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors()
+            )
+
+            ExposedDropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                tipOptions.forEach { option ->
+                    DropdownMenuItem(
+                        text = { Text(option) },
+                        onClick = {
+                            selectedTipOption = option
+                            expanded = false
+                        },
+                        contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                    )
+                }
+            }
+        }
+
+        Button(
+            onClick = {
+                totalBayar = calculateTotal(nominalBayar, selectedTipOption)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            Text(text = stringResource(R.string.calculate))
+        }
+
+        Button(
+            onClick = {
+                nominalBayar = ""
+                totalBayar = 0.0
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            Text(text = stringResource(R.string.reset))
+        }
+
+        Text(
+            text = "${stringResource(R.string.total_to_pay)}: Rp ${"%.2f".format(totalBayar)}",
+            modifier = Modifier.padding(top = 16.dp)
+        )
+        Button(
+            onClick = {},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            Text(text = stringResource(R.string.share))
+        }
+    }
+}
+
+fun calculateTotal(bill: String, tip: String): Double {
+    val billValue = bill.toDoubleOrNull() ?: 0.0
+    val tipPercentage = tip.replace("%", "").toDoubleOrNull() ?: 0.0
+    return billValue + (billValue * tipPercentage / 100)
 }
